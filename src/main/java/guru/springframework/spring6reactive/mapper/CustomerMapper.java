d1cd29647b0c77e5e4cffe502de4a079a5be96c5
Ech-Cherrate Ismail <echcherrate.ismail@gmail.com>
2026-03-16 18:29:54 +0100

    Date mapping when null

--- src/main/java/guru/springframework/spring6reactive/mapper/CustomerMapper.java
+++ src/main/java/guru/springframework/spring6reactive/mapper/CustomerMapper.java
@@ -9,11 +9,22 @@ package guru.springframework.spring6reactive.mapper;
 import guru.springframework.spring6reactive.domain.Customer;
 import guru.springframework.spring6reactive.model.CustomerDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.time.LocalDateTime;
 
 @Mapper
 public interface CustomerMapper {
 
+    @Mapping(target = "createdDate", expression = "java(customerDTO.getCreatedDate()!=null?customerDTO.getCreatedDate():java.time.LocalDateTime.now())")
+    @Mapping(target = "lastModifiedDate", expression = "java(resolveDate(customerDTO.getLastModifiedDate()))")
     Customer customerDTOtoCustomer(CustomerDTO customerDTO);
 
     CustomerDTO customerToCustomerDTO(Customer customer);
+
+
+    default LocalDateTime resolveDate(LocalDateTime date) {
+        return date != null ? date : LocalDateTime.now();
+    }
+
 }
